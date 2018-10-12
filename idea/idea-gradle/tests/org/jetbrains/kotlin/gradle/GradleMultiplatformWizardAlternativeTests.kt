cba5ab15ca0e8faf13046564dfe8b9ca370d2ad1
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2018-10-12 18:14:04 +0300

    MPP wizard test: add project import

--- idea/idea-gradle/tests/org/jetbrains/kotlin/gradle/GradleMultiplatformWizardAlternativeTests.kt
+++ idea/idea-gradle/tests/org/jetbrains/kotlin/gradle/GradleMultiplatformWizardAlternativeTests.kt
@@ -9,27 +9,72 @@ import com.intellij.ide.projectWizard.NewProjectWizard
 import com.intellij.ide.projectWizard.ProjectTypeStep
 import com.intellij.ide.projectWizard.ProjectWizardTestCase
 import com.intellij.ide.util.newProjectWizard.AbstractProjectWizard
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.Result
+import com.intellij.openapi.application.WriteAction
+import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.externalSystem.importing.ImportSpecBuilder
+import com.intellij.openapi.externalSystem.model.DataNode
+import com.intellij.openapi.externalSystem.model.project.ProjectData
+import com.intellij.openapi.externalSystem.service.execution.ProgressExecutionMode
+import com.intellij.openapi.externalSystem.service.project.ExternalProjectRefreshCallback
+import com.intellij.openapi.externalSystem.service.project.ProjectDataManager
+import com.intellij.openapi.externalSystem.settings.ExternalProjectSettings
+import com.intellij.openapi.externalSystem.settings.ExternalSystemSettingsListenerAdapter
+import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil
+import com.intellij.openapi.externalSystem.util.ExternalSystemUtil
 import com.intellij.openapi.module.ModuleManager
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.projectRoots.ProjectJdkTable
+import com.intellij.openapi.projectRoots.SimpleJavaSdkType
 import com.intellij.openapi.roots.ModuleRootManager
 import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.openapi.roots.ui.configuration.ModulesProvider
+import com.intellij.openapi.util.Couple
+import com.intellij.openapi.util.Ref
+import com.intellij.openapi.util.io.FileUtil
 import com.intellij.openapi.util.text.StringUtil
+import com.intellij.openapi.vfs.CharsetToolkit
+import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.vfs.VfsUtilCore
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.testFramework.IdeaTestUtil
 import com.intellij.testFramework.UsefulTestCase
+import com.intellij.util.containers.ContainerUtil
+import com.intellij.util.containers.ContainerUtilRt
 import junit.framework.TestCase
+import org.gradle.util.GradleVersion
 import org.jetbrains.kotlin.ide.konan.gradle.KotlinGradleNativeMultiplatformModuleBuilder
+import org.jetbrains.kotlin.idea.codeInsight.gradle.AbstractModelBuilderTest
+import org.jetbrains.kotlin.idea.codeInsight.gradle.GradleImportingTestCase
 import org.jetbrains.kotlin.idea.configuration.*
 import org.jetbrains.kotlin.utils.PrintingLogger
 import org.jetbrains.plugins.gradle.service.project.wizard.GradleModuleBuilder
+import org.jetbrains.plugins.gradle.settings.DistributionType
+import org.jetbrains.plugins.gradle.settings.GradleProjectSettings
+import org.jetbrains.plugins.gradle.settings.GradleSettings
+import org.jetbrains.plugins.gradle.util.GradleConstants
 import org.junit.Test
 import java.io.File
+import java.io.IOException
+import java.io.StringWriter
+import java.util.*
 
 class GradleMultiplatformWizardAlternativeTests : ProjectWizardTestCase<AbstractProjectWizard>() {
 
     override fun createWizard(project: Project?, directory: File): AbstractProjectWizard {
         return NewProjectWizard(project, ModulesProvider.EMPTY_MODULES_PROVIDER, directory.path)
+    }
+
+    private fun applyOnProjectSettings(f: GradleProjectSettings.() -> Unit) {
+        val systemSettings = ExternalSystemApiUtil.getSettings(myProject, externalSystemId)
+        val projectSettings = GradleProjectSettings()
+        projectSettings.f()
+        val projects = ContainerUtilRt.newHashSet<Any>(systemSettings.getLinkedProjectsSettings())
+        projects.remove(projectSettings)
+        projects.add(projectSettings)
+        systemSettings.setLinkedProjectsSettings(projects)
     }
 
     private fun testImportFromBuilder(
@@ -53,8 +98,13 @@ class GradleMultiplatformWizardAlternativeTests : ProjectWizardTestCase<Abstract
                 val projectBuilder = myWizard.projectBuilder
                 UsefulTestCase.assertInstanceOf(projectBuilder, builder::class.java)
                 (projectBuilder as GradleModuleBuilder).name = projectName
+
+                applyOnProjectSettings {
+                    distributionType = DistributionType.DEFAULT_WRAPPED
+                }
             }
         }
+        // TODO: do not use snapshot!
 
         TestCase.assertEquals(projectName, project.name)
         val modules = ModuleManager.getInstance(project).modules
@@ -74,13 +124,105 @@ class GradleMultiplatformWizardAlternativeTests : ProjectWizardTestCase<Abstract
 
         val buildScript = VfsUtilCore.findRelativeFile("build.gradle", root)!!
         println(StringUtil.convertLineSeparators(VfsUtilCore.loadText(buildScript)))
+
+        doImportProject()
     }
 
-    @Throws(Exception::class)
-    override fun setUp() {
-        super.setUp()
-        configureJdk()
+    @Throws(IOException::class)
+    private fun createProjectSubFile(relativePath: String): VirtualFile {
+        val f = File(myProject.basePath!!, relativePath)
+        FileUtil.ensureExists(f.parentFile)
+        FileUtil.ensureCanCreateFile(f)
+        val created = f.createNewFile()
+        if (!created) {
+            throw AssertionError("Unable to create the project sub file: " + f.absolutePath)
+        }
+        return LocalFileSystem.getInstance().refreshAndFindFileByIoFile(f)!!
     }
+
+    private fun createProjectSubFile(relativePath: String, content: String): VirtualFile {
+        val file = createProjectSubFile(relativePath)
+        runWrite {
+            file.setBinaryContent(content.toByteArray(CharsetToolkit.UTF8_CHARSET), file.modificationStamp, file.timeStamp)
+        }
+        return file
+    }
+
+    private fun runWrite(f: () -> Unit) {
+        object : WriteAction<Any>() {
+            override fun run(result: Result<Any>) {
+                f()
+            }
+        }.execute()
+    }
+
+    private fun doImportProject() {
+        ExternalSystemApiUtil.subscribe(
+            myProject,
+            GradleConstants.SYSTEM_ID,
+            object : ExternalSystemSettingsListenerAdapter<ExternalProjectSettings>() {
+                override fun onProjectsLinked(settings: Collection<ExternalProjectSettings>) {
+                    val item = ContainerUtil.getFirstItem<Any>(settings)
+                    if (item is GradleProjectSettings) {
+                        item.gradleJvm = DEFAULT_SDK
+                    }
+                }
+            })
+
+        GradleSettings.getInstance(myProject).gradleVmOptions = "-Xmx128m -XX:MaxPermSize=64m"
+        val wrapperJarFrom = LocalFileSystem.getInstance().refreshAndFindFileByIoFile(GradleImportingTestCase.wrapperJar())!!
+        val wrapperJarFromTo = createProjectSubFile("gradle/wrapper/gradle-wrapper.jar")
+        runWrite {
+            wrapperJarFromTo.setBinaryContent(wrapperJarFrom.contentsToByteArray())
+        }
+        val properties = Properties()
+        properties.setProperty("distributionBase", "GRADLE_USER_HOME")
+        properties.setProperty("distributionPath", "wrapper/dists")
+        properties.setProperty("zipStoreBase", "GRADLE_USER_HOME")
+        properties.setProperty("zipStorePath", "wrapper/dists")
+        val distributionUri = AbstractModelBuilderTest.DistributionLocator().getDistributionFor(GradleVersion.version("4.7"))
+        properties.setProperty("distributionUrl", distributionUri.toString())
+
+        val writer = StringWriter()
+        properties.store(writer, null)
+
+        createProjectSubFile("gradle/wrapper/gradle-wrapper.properties", writer.toString())
+
+        applyOnProjectSettings {
+            distributionType = DistributionType.DEFAULT_WRAPPED
+            externalProjectPath = myProject.basePath!!
+        }
+
+        val error = Ref.create<Couple<String>>()
+        ExternalSystemUtil.refreshProjects(
+            ImportSpecBuilder(myProject, externalSystemId)
+                .use(ProgressExecutionMode.MODAL_SYNC)
+                .callback(object : ExternalProjectRefreshCallback {
+                    override fun onSuccess(externalProject: DataNode<ProjectData>?) {
+                        if (externalProject == null) {
+                            System.err.println("Got null External project after import")
+                            return
+                        }
+                        ServiceManager.getService(ProjectDataManager::class.java).importData(externalProject, myProject, true)
+                        println("External project was successfully imported")
+                    }
+
+                    override fun onFailure(errorMessage: String, errorDetails: String?) {
+                        error.set(Couple.of(errorMessage, errorDetails))
+                    }
+                })
+                .forceWhenUptodate()
+        )
+
+        if (!error.isNull) {
+            var failureMsg = "Import failed: " + error.get().first
+            if (StringUtil.isNotEmpty(error.get().second)) {
+                failureMsg += "\nError details: \n" + error.get().second
+            }
+            TestCase.fail(failureMsg)
+        }
+    }
+
 
     @Test
     fun testMobile() {
@@ -105,5 +247,21 @@ class GradleMultiplatformWizardAlternativeTests : ProjectWizardTestCase<Abstract
     @Test
     fun testWeb() {
         testImportFromBuilder(KotlinGradleWebMultiplatformModuleBuilder(), "Web")
+    }
+
+    override fun setUp() {
+        super.setUp()
+        val javaHome = IdeaTestUtil.requireRealJdkHome()
+        ApplicationManager.getApplication().runWriteAction {
+            addSdk(SimpleJavaSdkType().createJdk(DEFAULT_SDK, javaHome))
+            addSdk(SimpleJavaSdkType().createJdk("_other", javaHome))
+
+            println("ProjectWizardTestCase.configureJdk:")
+            println(Arrays.asList(*ProjectJdkTable.getInstance().allJdks))
+        }
+    }
+
+    companion object {
+        val externalSystemId = GradleConstants.SYSTEM_ID
     }
 }
