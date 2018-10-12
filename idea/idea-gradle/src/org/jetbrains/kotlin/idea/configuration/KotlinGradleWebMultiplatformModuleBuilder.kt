1b1af7152288e254ae95b3038d5baa31b05bce43
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2018-10-12 13:20:47 +0300

    MPP wizards: remove unused module parameter from createProjectSkeleton

--- idea/idea-gradle/src/org/jetbrains/kotlin/idea/configuration/KotlinGradleWebMultiplatformModuleBuilder.kt
+++ idea/idea-gradle/src/org/jetbrains/kotlin/idea/configuration/KotlinGradleWebMultiplatformModuleBuilder.kt
@@ -29,7 +29,7 @@ class KotlinGradleWebMultiplatformModuleBuilder : KotlinGradleAbstractMultiplatf
     override fun getDescription() =
         "Multiplatform Gradle projects allow reusing the same Kotlin code between JS Client and JVM Server."
 
-    override fun createProjectSkeleton(module: Module, rootDir: VirtualFile) {
+    override fun createProjectSkeleton(rootDir: VirtualFile) {
         val src = rootDir.createChildDirectory(this, "src")
 
         val commonMain = src.createKotlinSampleFileWriter(commonSourceName)
