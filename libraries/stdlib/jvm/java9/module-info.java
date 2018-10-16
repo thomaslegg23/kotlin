e6a42315502881dd7f8b528c224052dea75d1122
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2018-10-16 13:45:34 +0300

    Build java9 modular artifacts under 'modular' classifier
    #KT-21266

--- libraries/stdlib/jvm/java9/module-info.java
+++ libraries/stdlib/jvm/java9/module-info.java
@@ -9,23 +9,26 @@ module kotlin.stdlib {
     exports kotlin.jvm.functions;
     exports kotlin.math;
     exports kotlin.properties;
+    // TODO: exports kotlin.random;
     exports kotlin.ranges;
     exports kotlin.reflect;
     exports kotlin.sequences;
     exports kotlin.system;
     exports kotlin.text;
 
-    exports kotlin.coroutines.experimental;
-    exports kotlin.coroutines.experimental.intrinsics;
-    exports kotlin.coroutines.experimental.jvm.internal;
+    exports kotlin.coroutines;
+    exports kotlin.coroutines.intrinsics;
+    exports kotlin.coroutines.jvm.internal;
+
+    // do not export kotlin.coroutines.experimental.*, otherwise it could create split package when moving these packages to a compatibility JAR
+    //exports kotlin.coroutines.experimental;
+    //exports kotlin.coroutines.experimental.intrinsics;
+    //exports kotlin.coroutines.experimental.jvm.internal;
     exports kotlin.experimental;
 
     exports kotlin.internal;
     exports kotlin.jvm.internal;
     exports kotlin.jvm.internal.markers;
-
-    // TODO?
-    // exports org.jetbrains.annotations;
 
     // Open packages with .kotlin_builtins files to kotlin-reflect, to allow reflection to load built-in declarations there
     opens kotlin to kotlin.reflect;
